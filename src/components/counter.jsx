6747fe14ad47b24fc2bddef0bd9e93c8399b2ff0
Qianqian Hu <qianhu@pindous.com>
2019-03-09 17:32:50 -0800

    add list to display product tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { link } from "fs";
 
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200"
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"]
   };
+  //   in state object we add one more property tags,in this array we will dynamically rerendering each string in an unordered list
+  // not as Angular which is a templating engine,  React is just a simple ayntax, get JavaScript code compelied into React element
+  // <span></span>
 
   styles = {
     fontSize: "10px",
@@ -15,13 +20,24 @@ class Counter extends Component {
     return (
       <div>
         <img src={this.state.imageUrl} alt="photo" />
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         {/* bootstrap class */}
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
